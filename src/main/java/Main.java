d2a7e34cb2362a5b4bb4786b670dbe8df51284dc
mohamednaji7 <n4jidx@gmail.com>
2025-07-15 14:20:34 +0300

    feat: implement RESP command parsing and handle PING/ECHO commands

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,8 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args){
@@ -38,24 +40,37 @@ public class Main {
         try{
             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);                   
             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            
-            String inputLine;
 
-            while ((inputLine = in.readLine()) != null) {
-                System.out.println("[LOG] Received: " + inputLine);
-                if (inputLine.trim().equalsIgnoreCase("PING")) {
-                    out.print("+PONG\r\n");
+            while (true) {
+                
+                List<String> command = parseRespCommand(in);
+                if (command.isEmpty()) {
+                    out.print("-ERR empty command\r\n");
                     out.flush();
-
+                    System.err.println("[ERROR] empty command");
+                    return;
                 }
-
-                // Break when we get an empty line or null (end of command)
-                if (inputLine.trim().isEmpty()) {
-                    break;
+    
+                String cmd = command.get(0).toUpperCase();
+                switch (cmd) {
+                    case "PING":
+                        out.print("+PONG\r\n");
+                        break;
+                    case "ECHO":
+                        System.out.println("[LOG] echo commands");
+                        String msg = command.get(1);
+                        System.out.println("[LOG] msg: " + msg );
+                        out.print("+" + msg + "\r\n");
+                        // out.print(msg);
+                        // out.print("$" + msg.length() + "\r\n" + msg + "\r\n");
+    
+                        break;
+    
+                    default:
+                        break;
                 }
-
+                out.flush();
             }
-   
 
 
         } catch (IOException e) {
@@ -68,4 +83,36 @@ public class Main {
             }
         }
     }
+
+    public static List<String> parseRespCommand(BufferedReader in) throws IOException {
+        System.out.println("[LOG] Parsing the RESP");
+
+        List<String> parts = new ArrayList<>();
+
+        String line = in.readLine(); // First line: *2
+        if (line == null || !line.startsWith("*")) {
+            return parts;
+        }
+
+        int numberOfParts = Integer.parseInt(line.substring(1)); 
+
+        for (int i = 0; i < numberOfParts; i++) {
+            String sizeLine = in.readLine(); // $4
+            if (sizeLine == null || !sizeLine.startsWith("$")) {
+                break;
+            }
+
+            // int size = Integer.parseInt(sizeLine.substring(1)); // 4
+            // char[] buf = new char[size];
+            // int read = in.read(buf, 0, size);
+            // in.readLine(); // skip \r\n after bulk string
+            // parts.add(new String(buf, 0, read));
+
+            String bulk = in.readLine();  // should be "ECHO" or "hey"
+            parts.add(bulk);
+        }
+
+        return parts;
+    }
+
 }
