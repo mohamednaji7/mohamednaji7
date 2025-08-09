32dae20d1828b541631c9e14f0f74b6a75b4d960
mohamednaji7 <n4jidx@gmail.com>
2025-08-10 01:42:46 +0300

    Add some chapters  9-15

---
+++ CH4/L9/main.js
@@ -0,0 +1,14 @@
+const campaign = {
+    getRemainingMessages() {
+      // ?
+      return this.maxMessages - this.sentMessages;
+    },
+    maxMessages: 100,
+    sentMessages: 30,
+    name: "Welcome Campaign",
+  };
+  
+  // don't touch below this line
+  
+  export { campaign };
+  
