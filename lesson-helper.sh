db337555ddb7490bb409da4ae41114f8b334b29c
mohamednaji7 <n4jidx@gmail.com>
2025-08-09 19:48:12 +0300

    Refactor lesson-helper.sh: simplify parameter handling and add 'create' alias
    - Allow 'create' as alias for parameter '1'
    - Remove separate test option '2', make testing the default action

--- lesson-helper.sh
+++ lesson-helper.sh
@@ -9,20 +9,15 @@ if [ -z "$1" ] || [ -z "$2" ]; then
   exit 1
 fi
 
-if [ "$3" = "1" ]; then
+if [ "$3" = "1" ] || [ "$3" = "create" ] ; then
     echo "Creating lesson directory structure..."
     mkdir "./$1/L$2"
     cp "./Lesson tmp/"* "$1/L$2/"
-elif [ "$3" = "2" ]; then
-    echo "running test..."
-
-    node ./$1/L$2/main_test.js --submit
-
 elif [ "$3" = "re-init" ]; then
     echo "Re-init lesson directory structure..."
     mkdir -p "./$1/L$2"
     cp "./Lesson tmp/"* "$1/L$2"
 else
-    echo "Invalid option. Use 1 to create, 2 to test, or 're-init'."
-    exit 1
+    echo "running test..."
+    node ./$1/L$2/main_test.js --submit
 fi
