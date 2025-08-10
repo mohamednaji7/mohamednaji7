0ce5299dd37d29d34ca8ae631f9329779b1ae9f1
mohamednaji7 <n4jidx@gmail.com>
2025-08-11 02:46:07 +0300

    One source of truth for the lesson helper

--- make-lesson.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo  "You passed: $1"
-echo  "You passed: 2: $2"
-echo "Creating lesson directory structure..."
-#  if mkdir errors (File exists), the cp and rm won’t execute.
-mkdir "./$1/L$2" &&
-    cp "./Lesson tmp/"* "$1/L$2/" &&
-        rm ./$1/L$2/unit_test.js
--- test-lesson.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo  "You passed: $1"
-echo  "You passed: 2: $2"
-
-echo "running test..."
-cp "./Lesson tmp/unit_test.js" ./$1/L$2/
-node ./$1/L$2/main_test.js --submit
-rm ./$1/L$2/unit_test.js
