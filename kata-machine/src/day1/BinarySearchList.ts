c21dbd2b378a6cbf50c5288caf0c34fbe11e3eb2
mohamednaji7 <n4jidx@gmail.com>
2025-08-11 16:24:34 +0300

    Solve the BinarySearchList.ts

--- kata-machine/src/day1/BinarySearchList.ts
+++ kata-machine/src/day1/BinarySearchList.ts
@@ -1,3 +1,16 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-
+    let lo = 0;
+    let hi = haystack.length-1;
+    while( lo <= hi ){
+        const m = Math.floor((lo+hi)/2)
+        const mv = haystack[m];
+        if(mv===needle){
+            return true;
+        }else if(mv < needle ){
+            lo = m + 1;
+        }else{
+            hi = m-1;
+        }
+    }
+    return false;
 }
